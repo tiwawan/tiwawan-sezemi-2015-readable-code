4941bbe0d9bf7b40930d161ecd4fe5329216b227
tiwawan <suken63@gmail.com>
2015-06-06 11:29:10 +0900

    spec3: データファイルを分離

--- recipe.c
+++ recipe.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]){
-  printf("オムライス\n");
+  FILE *f_recipe = fopen(argv[1], "r");
+  char recipename[100];
+
+  fgets(recipename, 100, f_recipe);
+
+  printf(recipename);
+
   return 0;
 }
